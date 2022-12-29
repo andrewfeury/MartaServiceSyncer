d8c6eac3fa39aa8be66c923e5d5afb8abc0ed4ae
Andrew Feury <andrew@feury.us>
2022-12-29 13:27:42 -0500

    Query function

---
+++ src/functions/querybusalerts/src/main/java/us/feury/martasync/MartaQueryFunction.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package us.feury.martasync;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.DescribeTableRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
+import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
+import software.amazon.awssdk.services.dynamodb.model.ScanRequest;
+import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
+import software.amazon.awssdk.services.dynamodb.model.TableDescription;
+
+/**
+ * Lambda function entry point.
+ * 
+ * @author Andrew Feury
+ * @see <a href=https://docs.aws.amazon.com/lambda/latest/dg/java-handler.html>Lambda Java Handler</a>
+ * for more information
+ */
+public class MartaQueryFunction implements RequestHandler<MartaQueryInput, MartaQueryOutput> {
+    
+    // Constants
+    private static final Region REGION = Region.US_EAST_1;
+    private static final String DYNAMODB_TABLE_NAME = "ActiveAlerts";
+    
+    // Clients
+    private final DynamoDbClient dynamoDbClient;
+
+    // Logger
+    private static final Logger log = LoggerFactory.getLogger(MartaQueryFunction.class);
+
+    public MartaQueryFunction() {
+        
+        // Init DynamoDB client & verify table exists
+        this.dynamoDbClient = 
+                DynamoDbClient.builder()
+                              .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                              .region(REGION)
+                              .httpClientBuilder(UrlConnectionHttpClient.builder())
+                              .build();
+        TableDescription table = 
+                this.dynamoDbClient.describeTable(
+                                        DescribeTableRequest.builder()
+                                                            .tableName(DYNAMODB_TABLE_NAME)
+                                                            .build()
+                                    ).table();
+        if (log.isDebugEnabled()) log.debug("Table found: {}", table.tableId());
+    }
+
+    @Override
+    public MartaQueryOutput handleRequest(final MartaQueryInput input, final Context context) {
+        
+        Optional<String> route = Optional.ofNullable(input.getRoute());
+        if (route.isPresent()) {
+            return queryTweetsByRoute(route.get());
+        } else {
+            return queryTweetsAll();
+        }
+    }
+
+    private MartaQueryOutput queryTweetsByRoute(String route) {
+
+        // Build a query for the route with created time descending
+        QueryRequest query = 
+                QueryRequest.builder()
+                            .tableName(DYNAMODB_TABLE_NAME)
+                            .keyConditionExpression(route)
+                            .scanIndexForward(false)
+                            .build();
+        QueryResponse response = this.dynamoDbClient.query(query);
+
+        // Parse response
+        final MartaQueryOutput result = new MartaQueryOutput();
+        response.items().forEach(m->parseToResult(m,result));
+        return result;
+    }
+
+    private void parseToResult(Map<String, AttributeValue> itemData, MartaQueryOutput result) {
+        
+        String route =
+                Optional.ofNullable(itemData.get("Route"))
+                        .map(AttributeValue::s)
+                        .orElse("Unknown");
+
+        String created =
+                Optional.ofNullable(itemData.get("Created"))
+                        .map(AttributeValue::n)
+                        .map(Long::decode)
+                        .map(l->Instant.ofEpochSecond(l).toString())
+                        .orElse("");
+
+        String text =
+                Optional.ofNullable(itemData.get("Text"))
+                        .map(AttributeValue::s)
+                        .orElse("");
+
+        result.putTweet(
+                    route,
+                    created,
+                    text
+                );
+    }
+
+    private MartaQueryOutput queryTweetsAll() {
+        
+        // Build a scan (all-item query) with created time descending
+        final ScanRequest scan =
+                ScanRequest.builder()
+                           .tableName(DYNAMODB_TABLE_NAME)
+                           .build();
+        ScanResponse response = this.dynamoDbClient.scan(scan);
+
+        // Parse response
+        final MartaQueryOutput result = new MartaQueryOutput();
+        response.items().forEach(m->parseToResult(m,result));
+        return result;          
+    }
+}
---
+++ src/functions/querybusalerts/src/main/java/us/feury/martasync/MartaQueryOutput.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2002-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package us.feury.martasync;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MartaQueryOutput {
+    
+    private Map<String, MartaServiceTweet> tweetsByRoute = new LinkedHashMap<>();
+
+    public Map<String, MartaServiceTweet> getTweetsByRoute() {
+        return tweetsByRoute;
+    }
+
+    public MartaServiceTweet putTweet(String route, String createdAt, String text) {
+        MartaServiceTweet tweet = new MartaServiceTweet(createdAt, text);
+        return tweetsByRoute.merge(route, tweet, this::mergeTweets);
+    }
+
+    private MartaServiceTweet mergeTweets(MartaServiceTweet existing, MartaServiceTweet update) {
+        
+        if (LocalDateTime.parse(update.getLastUpdated())
+                .isAfter(LocalDateTime.parse(existing.getLastUpdated()))) {
+            // New tweet is more recent
+            String newText = String.join("\n\n", update.getText(), existing.getText());
+            return new MartaServiceTweet(update.getLastUpdated(), newText);
+        } else {
+            // Existing tweet is more recent
+            String newText = String.join("\n\n", existing.getText(), update.getText());
+            return new MartaServiceTweet(existing.getLastUpdated(), newText);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("MartaQueryOutput [tweetsByRoute=%s]", tweetsByRoute);
+    }
+}
